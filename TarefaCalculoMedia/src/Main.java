b0a4ae8d362829fad17fc51acb7907301a4b5a46
JuniorTemp <junior_sadaanzinhoa@hotmail.com>
2023-11-30 14:49:15 -0300

    first commit

---
+++ TarefaCalculoMedia/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Calcular Média de notas
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        int num5;
+        //Notas
+
+        num1 = 8;
+        num2 = 7;
+        num3 = 6;
+        num4 = 7;
+        //Calculo total das notas
+
+        num5 = num1 + num2 + num3 + num4;
+        System.out.println(num5);
+
+        //Divisao da media total
+
+        System.out.println(num5/4);
+
+
+
+
+    }
+}
